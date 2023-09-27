3c632851aae44a811937266fc34db216fb68954a
jlio215 <jlio215@hotmail.com>
2023-09-27 09:05:13 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const { mongoConn } = require('./databases/configuration')
+const dotenv = require('dotenv').config()
+
+mongoConn()
+
+const app = express()
+
+app.use(express.json())
+
+const generos = require('./routes/genero')
+const productora = require('./routes/productora')
+const media = require('./routes/media')
+const director = require('./routes/director')
+const tipo = require('./routes/tipo')
+
+
+app.use('/api/v1/generos', generos)
+app.use('/api/v1/productora', productora)
+app.use('/api/v1/media', media)
+app.use('/api/v1/director', director)
+app.use('/api/v1/tipo', tipo)
+
+module.exports = app
