e0478c36feca35b7e779b39753469ba873b2ec9d
jlio215 <31058099+jlio215@users.noreply.github.com>
2023-10-16 14:37:04 -0500

    cors

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const { mongoConn } = require('./databases/configuration')
 const dotenv = require('dotenv').config()
+const cors = require('cors')
 
 mongoConn()
 
 const app = express()
 
 app.use(express.json())
+
+app.use(cors({
+  origin: '*'
+}))
 
 const generos = require('./routes/genero')
 const productora = require('./routes/productora')
